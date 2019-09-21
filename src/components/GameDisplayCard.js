2b73b90f7ced1b1a34d5c567ef23466710df97b2
nicholasstano <nicholasstano@gmail.com>
2019-09-21 14:18:37 -0400

    update game backend 1.0

--- src/components/GameDisplayCard.js
+++ src/components/GameDisplayCard.js
@@ -5,8 +5,8 @@ export class GameDisplayCard extends Component {
         return (
             <div >
                 <div className="home-game-display">
-                    <p><img src={this.props.away_flag} alt={this.props.away_name} />{this.props.away_name}</p>
-                    <p><img src={this.props.home_flag} alt={this.props.home_name} />{this.props.home_name}</p>
+                    <p><img src={this.props.away_flag} alt={this.props.away_name} /> {this.props.away_name}</p>
+                    <p><img src={this.props.home_flag} alt={this.props.home_name} /> {this.props.home_name}</p>
                 </div>
             </div>
         )
